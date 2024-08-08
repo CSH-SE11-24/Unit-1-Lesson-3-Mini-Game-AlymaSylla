abd1d07a19bdfd92d90bfd80abf2e9a63af09060
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 10:25:25 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// Get started!
+console.log("Hello, thank  you for joining my game.Its time to see if you are considered a true Munchkin by playing my game, ")
+let name=prompt("What is your name?")
+console.log("welcome to my game, " + name)
+
+let role=prompt("who was ice spice first feature? (lil tjay,pink pantheress,nicki minaj)")
+console.log(`Okay, ${name}, your role is now ${role}.`)
